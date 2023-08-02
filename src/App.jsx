21a5904dbd7af5619e1c38ef523e278523538f6b
Bubekk <sebastian.kubanski@gmail.com>
2023-08-02 03:10:42 +0200

    First glance and first idea

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import LineRow from "./components/LineRow/LineRow";
+import MainFunction from "./components/Main/MainFunction";
+
+function App() {
+  return (
+    <>
+      <LineRow number="1" content="first" />
+      <LineRow number="2" content="mainfunction" />
+    </>
+  );
+}
+
+export default App;
